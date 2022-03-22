501ace3d06def064668d37e268624f0d61e5139b
MuhammedKaplan0 <100554179+MuhammedKaplan0@users.noreply.github.com>
2022-03-22 09:12:12 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Label = System.Reflection.Emit.Label;
+
+namespace ToolTip_Uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     ToolTip tt = new ToolTip();
+  
+  tt.SetToolTip(label1, "Adınızı giriniz.");
+  tt.SetToolTip(label2, "Soyadınızı giriniz.");
+  tt.SetToolTip(label3, "Doğum tarihinizi gg/aa/yyyy şeklinde giriniz.");
+  tt.SetToolTip(label4, "Adresinizi giriniz.");
+  tt.SetToolTip(button1, "Kaydetmek için tıklayınız.");
+        }
+    }
+}
